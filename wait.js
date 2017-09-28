4c730f936d35fe7ab007c7bb4b21892ebb6b1626
Reetesh Ranjan <reeteshranjan@piaxis.tech>
2017-09-28 18:14:23 +0530

    File write in place of console log

--- wait.js
+++ wait.js
@@ -1,12 +1,18 @@
 'use strict'
 
-setTimeout(() => { console.log('timed out!') }, 50000)
+let fs = require('fs'),
+  path = require('path'),
+  file = path.resolve(process.cwd(), 'out.txt')
+
+setTimeout(() => { fs.writeFileSync(file, 'timed out!') }, 50000)
 
 let signals = ['SIGINT', 'SIGTERM', 'SIGHUP']
 
 signals.forEach(function(sig) {
   process.on(sig, function() {
-    console.log('received exit signal %s', sig)
+    fs.writeFileSync(file, 'received exit signal ' + sig)
     process.exit(0)
   })
 })
+
+console.log('Started waiting...')
